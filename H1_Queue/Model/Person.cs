cfa671b7ec192a8edf8d091068910a212d3b670b
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-09-14 12:03:26 +0200

    update person class

--- H1_Queue/Model/Person.cs
+++ H1_Queue/Model/Person.cs
@@ -8,6 +8,23 @@ namespace H1_Queue.Model
 {
     internal class Person
     {
+        internal readonly string[] firstNames = { "John", "Paul", "Ringo", "George", "Stephen", "Larry", "Scott", "Alexander", "Dennis", "Tyler" };
 
+        internal readonly string[] lastNames = { "Lennon", "McCartney", "Starr", "Harrison", "Anderson", "Moore", "Lee", "Thompson", "Clark", "Hill" };
+
+        internal string firstName;
+        internal string lastName;
+
+        internal string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        internal string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
     }
 }
